eabc5400bade85513f15e5393f22312f43e1d006
Brian Hsu <brian@brianhsu.me>
2020-03-07 18:36:22 -0800

    Added authentication

---
+++ app/api/routes.py
@@ -0,0 +1,19 @@
+from app.api import bp
+from app.api.auth import token_auth
+
+
+@bp.route('/get_entry/<int:id>', methods=['GET'])
+@token_auth.login_required
+def get_entry(id):
+    # TODO: We need to check if
+    # - The specified entry exists
+    # - The current user owns that entry
+    pass
+
+
+@bp.route('/get_entries/<int:id>', methods=['GET'])
+@token_auth.login_required
+def get_entries():
+    # TODO: Get the entries for the current user. Probably a list of ints.
+    # Also, check to make sure that the specified ID equals the current user's ID.
+    pass
--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
+import os
+import base64
 from app import db
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -6,9 +10,38 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        db.session.commit()
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
+
+def __repr__(self):
+    return '<User {}>'.format(self.username)
 
 
 class JournalEntry(db.Model):
